cdf28a0629653c4ae6e5b7f3c4eb1d9d3a4a4d5a
João Victor R <joaovrs.com10@hotmail.com>
2024-04-14 23:25:06 -0300

    Criação da Janela

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import Tk, ttk
+
+# cores --------------------------
+co0 = "#f0f3f5"  # Preta / black
+co1 = "#feffff"  # branca / white
+co2 = "#3fb5a3"  # verde / green
+co3 = "#38576b"  # valor / value
+co4 = "#403d3d"   # letra / letters
+
+# Janelas --------------------------
+janela = Tk()
+janela.title('')
+janela.geometry('310x300')
+janela.configure(background=co1)
+janela.resizable(width=FALSE, height=FALSE)
+
+janela.mainloop()
