15f8ffa7e3e1a0b7d8092841c59804871caf67b6
João Victor R <joaovrs.com10@hotmail.com>
2024-04-14 23:35:00 -0300

    Configurando parte de cima

--- main.py
+++ main.py
@@ -15,4 +15,16 @@ janela.geometry('310x300')
 janela.configure(background=co1)
 janela.resizable(width=FALSE, height=FALSE)
 
+# Dividindo a janela -------------------------------
+frame_cima = Frame(janela, width=310, height=50, bg=co1, relief='flat')
+frame_cima.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
+
+frame_baixo= Frame(janela, width=310, height=250, bg=co2, relief='flat')
+frame_baixo.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+
+# Configurando frame de cima ----------------------------------
+l_nome = Label(frame_cima, text='LOGIN', anchor=NE,font=('Ivy 25'), bg=co1, fg=co4)
+l_nome.place(x=5, y=5)
+
 janela.mainloop()
